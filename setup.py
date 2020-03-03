ad5ab8ea1377d1bf5c6f1f261d40569259062e8f
alexander.rozhkov <alexander.rozhkov@nordigy.ru>
2020-03-03 12:29:49 +0300

    Add python packaging

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rc_client",  # Replace with your own username
+    version="0.0.1",
+    author="Aleksandr Rozhkov",
+    author_email="alexander.rozhkov@nordigy.ru",
+    description="Ringcentral API wrapper for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.ringcentral.com/rcvdevops/rc_sdk.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
